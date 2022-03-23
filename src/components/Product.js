9928bfcb126633430bfae54095aa4c988ddd35f8
edabicakci <d.edabicakci@gmail.com>
2022-03-23 16:55:23 +0300

    minor changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -29,19 +29,13 @@ const Product = (props) => {
     setCart([...updatedCart]);
   };
 
-  
   return (
     <>
       <div className="product" onClick={showProductDetail}>
         <p>{product.name}</p>
         <p>Unit Price: {product.unitPrice}</p>
         <p>Stock: {product.unitsInStock}</p>
-        {isInCart ?
-        <> 
-        <p> Piece: {product.count} </p> 
-        <p> Total Price: {(product.count * product.unitPrice).toFixed(2)} TL</p> 
-        </>
-         : null}
+        {isInCart ? <p> Total Price: {(product.count * product.unitPrice).toFixed(2)}</p> : null}
       </div>
 
       <div>
